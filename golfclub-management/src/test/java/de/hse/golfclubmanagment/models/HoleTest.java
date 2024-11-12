d47170f8d61f0cf317950ddecb2af68e602068d0
Daniel <daniel@familie-ryssel.de>
2024-11-12 14:29:05 +0100

    setup for testing

---
+++ golfclub-management/src/test/java/de/hse/golfclubmanagment/models/HoleTest.java
@@ -0,0 +1,12 @@
+package de.hse.golfclubmanagment.models;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class HoleTest {
+    @Test
+    public void tmp(){
+        assertEquals(0, 0);
+    }
+}
