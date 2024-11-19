be83b49cbc4c012bfd2949913b4deac7be94b146
Daniel <daniel@familie-ryssel.de>
2024-11-19 16:57:02 +0100

    optimized version for the tournament tests

--- golfclub-management/src/test/java/de/hse/golfclubmanagement/models/TournamentTest.java
+++ golfclub-management/src/test/java/de/hse/golfclubmanagement/models/TournamentTest.java
@@ -1,58 +1,49 @@
 package de.hse.golfclubmanagement.models;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import java.util.Date;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TournamentTest {
-    @Test
-    void testGetDate() {
-        Date date = new Date();
-        Tournament tournament = new Tournament();
-        tournament.setDate(date);
-        assertEquals(date, tournament.getDate());
 
+    private Tournament tournament;
 
+    @BeforeEach
+    void setUp() {
+        tournament = new Tournament();
     }
 
     @Test
-    void testGetId() {
-        Tournament tournament = new Tournament();
+    void testIdField() {
+        
         tournament.setId(1L);
         assertEquals(1L, tournament.getId());
     }
 
     @Test
-    void testGetName() {
-        Tournament tournament = new Tournament();
+    void testNameField() {
+        
         tournament.setName("Esslingen");
         assertEquals("Esslingen", tournament.getName());
 
+       
+        tournament.setName(null);
+        assertNull(tournament.getName());
     }
 
     @Test
-    void testSetDate() {
-        Tournament tournament = new Tournament();
+    void testDateField() {
+        
         Date date = new Date();
         tournament.setDate(date);
         assertEquals(date, tournament.getDate());
 
-    }
-
-    @Test
-    void testSetId() {
-        Tournament tournament = new Tournament();
-        tournament.setId(1L);
-        assertEquals(1L, tournament.getId());
-    }
-
-    @Test
-    void testSetName() {
-        Tournament tournament = new Tournament();
-        tournament.setName("Esslingen");
-        assertEquals("Esslingen", tournament.getName());
-
+       
+        tournament.setDate(null);
+        assertNull(tournament.getDate());
     }
 }
